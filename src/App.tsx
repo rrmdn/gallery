2e558e8247de83a3628342d0706cec8772870696
Rizki Romadhoni <rrmdn@protonmail.com>
2024-10-30 06:11:43 +0100

    initialize project

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <input
+        type="search"
+        name="search"
+        placeholder="Search"
+        aria-label="Search"
+      />
+      <h1>Heading 1</h1>
+      <h2>Heading 2</h2>
+      <h3>Heading 3</h3>
+      <h4>Heading 4</h4>
+      <h5>Heading 5</h5>
+      <h6>Heading 6</h6>
+      <p>Paragraph before the horizontal line.</p>
+      <hr />
+      <p>Paragraph after the horizontal line.</p>
+    </>
+  );
+}
+
+export default App;
