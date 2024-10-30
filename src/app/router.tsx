96620bfa27e23282974929eddfd76aff50f3abfd
Rizki Romadhoni <rrmdn@protonmail.com>
2024-10-30 21:50:00 +0100

    remember last scroll position

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -1,15 +1,64 @@
-import { createBrowserRouter } from "react-router-dom";
+import { Outlet, createBrowserRouter, useLocation } from "react-router-dom";
 import PhotosHomePage from "./photos/home.page";
 import PhotoDetailPage from "./photos/detail.page";
+import { useEffect, useMemo } from "react";
+
+const RememberMaxScroll = () => {
+  const location = useLocation();
+  const scrollHeightByPath = useMemo(() => {
+    return new Map<string, number>();
+  }, []);
+  useEffect(
+    function restoreScroll() {
+      const scrollHeight = scrollHeightByPath.get(location.pathname);
+      if (scrollHeight) {
+        setTimeout(() => {
+          window.scrollTo(0, scrollHeight);
+        }, 0);
+      }
+    },
+    [location.pathname, scrollHeightByPath]
+  );
+
+  useEffect(
+    function listenScroll() {
+      function handleScroll() {
+        scrollHeightByPath.set(location.pathname, window.scrollY);
+      }
+      window.addEventListener("scroll", handleScroll);
+      return () => {
+        window.removeEventListener("scroll", handleScroll);
+      };
+    },
+    [location.pathname, scrollHeightByPath]
+  );
+
+  return null;
+};
+
+const App = () => {
+  return (
+    <>
+      <Outlet />
+      <RememberMaxScroll />
+    </>
+  );
+};
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <PhotosHomePage />,
-  },
-  {
-    path: "/photos/:id",
-    element: <PhotoDetailPage />,
+    element: <App />,
+    children: [
+      {
+        index: true,
+        element: <PhotosHomePage />,
+      },
+      {
+        path: "/photos/:id",
+        element: <PhotoDetailPage />,
+      },
+    ],
   },
 ]);
 
